8cff689f2214afbede5959ddb47909d24d57b445
Lakhan Samani <lakhan.m.samani@gmail.com>
2021-07-08 17:45:19 +0530

    update server boilerplate to use graphql

--- server/handler/root.go
+++
@@ -1,8 +0,0 @@
-package handler
-
-import "github.com/gofiber/fiber/v2"
-
-// Root hanlde api status
-func Root(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{"status": "success", "message": "Hello you have reached yauth server", "data": nil})
-}
