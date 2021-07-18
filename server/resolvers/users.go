c6cbcd2e66df03eda61801500c6d8a1aec2db69e
Lakhan Samani <lakhan.m.samani@gmail.com>
2021-07-18 09:37:08 +0530

    Add super admin validation
    Resolves #24

--- server/resolvers/users.go
+++ server/resolvers/users.go
@@ -6,10 +6,20 @@ import (
 
 	"github.com/yauthdev/yauth/server/db"
 	"github.com/yauthdev/yauth/server/graph/model"
+	"github.com/yauthdev/yauth/server/utils"
 )
 
 func Users(ctx context.Context) ([]*model.User, error) {
+	gc, err := utils.GinContextFromContext(ctx)
 	var res []*model.User
+	if err != nil {
+		return res, err
+	}
+
+	if !utils.IsSuperAdmin(gc) {
+		return res, fmt.Errorf("unauthorized")
+	}
+
 	users, err := db.Mgr.GetUsers()
 	if err != nil {
 		return res, err
