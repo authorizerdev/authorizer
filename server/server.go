885aaab6ee522206ac446a6c8ebba7814c363f2e
Lakhan Samani <lakhan.m.samani@gmail.com>
2021-07-21 03:34:03 +0530

    Implement verify email using server url
    - fix confirm password typo

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"log"
 
 	"github.com/gin-gonic/gin"
 	"github.com/yauthdev/yauth/server/enum"
@@ -17,11 +18,31 @@ func GinContextToContextMiddleware() gin.HandlerFunc {
 	}
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		origin := c.Request.Header.Get("Origin")
+		log.Println("-> origin", origin)
+		c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	r := gin.Default()
 	r.Use(GinContextToContextMiddleware())
+	r.Use(CORSMiddleware())
 	r.GET("/", handlers.PlaygroundHandler())
 	r.POST("/graphql", handlers.GraphqlHandler())
+	r.GET("/verify_email", handlers.VerifyEmail())
 	if oauth.OAuthProvider.GoogleConfig != nil {
 		r.GET("/login/google", handlers.HandleOAuthLogin(enum.GoogleProvider))
 		r.GET("/callback/google", handlers.HandleOAuthCallback(enum.GoogleProvider))
