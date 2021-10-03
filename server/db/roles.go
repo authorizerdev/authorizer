d8092e0ae3b7899b1e79e6640da6074ccde05b38
Lakhan Samani <lakhan.m.samani@gmail.com>
2021-10-03 21:33:55 +0530

    feat: use uuid instead of unit type ids

--- server/db/roles.go
+++ server/db/roles.go
@@ -1,15 +1,31 @@
 package db
 
-import "log"
+import (
+	"log"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
 
 type Role struct {
-	ID   uint `gorm:"primaryKey"`
-	Role string
+	ID   uuid.UUID `gorm:"type:uuid;"`
+	Role string    `gorm:"unique"`
+}
+
+func (r *Role) BeforeCreate(tx *gorm.DB) (err error) {
+	r.ID = uuid.New()
+
+	return
 }
 
 // SaveRoles function to save roles
 func (mgr *manager) SaveRoles(roles []Role) error {
-	res := mgr.db.Create(&roles)
+	res := mgr.db.Clauses(
+		clause.OnConflict{
+			OnConstraint: "authorizer_roles_role_key",
+			DoNothing:    true,
+		}).Create(&roles)
 	if res.Error != nil {
 		log.Println(`Error saving roles`)
 		return res.Error
