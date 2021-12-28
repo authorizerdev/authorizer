4c53eb97d28a909dc733219667bb541484dd6ca8
Lakhan Samani <lakhan.m.samani@gmail.com>
2021-12-29 04:16:31 +0530

    fix: enable code spliting for app

--- app/src/pages/rest-password.tsx
+++ app/src/pages/rest-password.tsx
@@ -2,11 +2,11 @@ import React, { Fragment } from 'react';
 import { AuthorizerResetPassword } from '@authorizerdev/authorizer-react';
 
 export default function ResetPassword() {
-  return (
-    <Fragment>
-      <h1 style={{ textAlign: 'center' }}>Reset Password</h1>
-      <br />
-      <AuthorizerResetPassword />
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<h1 style={{ textAlign: 'center' }}>Reset Password</h1>
+			<br />
+			<AuthorizerResetPassword />
+		</Fragment>
+	);
 }
