3cb02dd62c6e63b2692f57043624c264f703b854
Lakhan Samani <lakhan.m.samani@gmail.com>
2022-01-19 22:43:07 +0530

    fix(dashboard): update home page text

--- dashboard/src/pages/Environment.tsx
+++ dashboard/src/pages/Environment.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import React from 'react';
 
 export default function Environment() {
--- dashboard/src/pages/Home.tsx
+++ dashboard/src/pages/Home.tsx
@@ -1,11 +1,18 @@
-import { Box } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
 import React from 'react';
 
 export default function Home() {
 	return (
-		<Box>
-			Hi there 👋 <br />
-			Welcome to Authorizer administrative dashboard!
-		</Box>
+		<>
+			<Text fontSize="2xl" fontWeight="bold">
+				Hi there 👋 <br />
+			</Text>
+
+			<Text fontSize="xl" color="gray.700">
+				Welcome to Authorizer Administrative Dashboard! <br />
+				Please use this dashboard to configure your environment variables or
+				have look at your users
+			</Text>
+		</>
 	);
 }
