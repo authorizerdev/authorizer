d5f1c5a5eb7fa2a41fcdcf572961bc98bb675b05
Lakhan Samani <lakhan.m.samani@gmail.com>
2022-04-02 17:34:50 +0530

    Resolves #156

--- server/db/models/verification_requests.go
+++ server/db/models/verification_requests.go
@@ -7,11 +7,11 @@ type VerificationRequest struct {
 	Key         string `json:"_key,omitempty" bson:"_key"` // for arangodb
 	ID          string `gorm:"primaryKey;type:char(36)" json:"_id" bson:"_id"`
 	Token       string `gorm:"type:text" json:"token" bson:"token"`
-	Identifier  string `gorm:"uniqueIndex:idx_email_identifier" json:"identifier" bson:"identifier"`
+	Identifier  string `gorm:"uniqueIndex:idx_email_identifier;type:varchar(64)" json:"identifier" bson:"identifier"`
 	ExpiresAt   int64  `json:"expires_at" bson:"expires_at"`
 	CreatedAt   int64  `json:"created_at" bson:"created_at"`
 	UpdatedAt   int64  `json:"updated_at" bson:"updated_at"`
-	Email       string `gorm:"uniqueIndex:idx_email_identifier" json:"email" bson:"email"`
+	Email       string `gorm:"uniqueIndex:idx_email_identifier;type:varchar(256)" json:"email" bson:"email"`
 	Nonce       string `gorm:"type:text" json:"nonce" bson:"nonce"`
 	RedirectURI string `gorm:"type:text" json:"redirect_uri" bson:"redirect_uri"`
 }
