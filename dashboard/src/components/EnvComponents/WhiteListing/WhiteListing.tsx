b8cf54a02ead832aa29b03b17e1d2332e4ab5f95
akash.dutta <akash.dutta@glosity.club>
2022-04-27 21:17:35 +0530

    org info  page responsive

--- dashboard/src/components/EnvComponents/WhiteListing/WhiteListing.tsx
+++ dashboard/src/components/EnvComponents/WhiteListing/WhiteListing.tsx
@@ -1,32 +1,24 @@
 import React from "react";
-import {
-  Box,
-  Divider,
-  Flex,
-  Stack,
-  Center,
-  Text,
-  Button,
-  Input,
-  InputGroup,
-  InputRightElement,
-  useToast,
-} from "@chakra-ui/react";
+import { Flex, Stack, Center, Text, useMediaQuery } from "@chakra-ui/react";
 import InputField from "../../InputField";
 import { ArrayInputType } from "../../../constants";
 const WhiteListing = ({ variables, setVariables }: any) => {
+  const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
   return (
     <div>
       {" "}
-      <Text fontSize="md" paddingTop="2%" fontWeight="bold">
+      <Text fontSize="md" paddingTop="2%" fontWeight="bold" mb={5}>
         White Listing
       </Text>
       <Stack spacing={6} padding="2% 0%">
-        <Flex>
+        <Flex direction={isNotSmallerScreen ? "row" : "column"}>
           <Flex w="30%" justifyContent="start" alignItems="center">
             <Text fontSize="sm">Allowed Origins:</Text>
           </Flex>
-          <Center w="70%">
+          <Center
+            w={isNotSmallerScreen ? "70%" : "100%"}
+            mt={isNotSmallerScreen ? "0" : "3"}
+          >
             <InputField
               variables={variables}
               setVariables={setVariables}
