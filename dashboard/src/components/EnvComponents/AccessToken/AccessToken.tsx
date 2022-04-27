f1d902c8922b957bb06be1fc29c79b289175db92
akash.dutta <akash.dutta@glosity.club>
2022-04-27 21:29:39 +0530

    org info  page responsive

--- dashboard/src/components/EnvComponents/AccessToken/AccessToken.tsx
+++ dashboard/src/components/EnvComponents/AccessToken/AccessToken.tsx
@@ -1,22 +1,31 @@
 import React from "react";
-import { Flex, Stack, Text } from "@chakra-ui/react";
+import { Flex, Stack, Text, useMediaQuery } from "@chakra-ui/react";
 import InputField from "../../InputField";
 import { TextInputType, TextAreaInputType } from "../../../constants";
 
 const AccessToken = ({ variables, setVariables }: any) => {
+  const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
   return (
     <div>
       {" "}
-      <Text fontSize="md" paddingTop="2%" fontWeight="bold">
+      <Text fontSize="md" paddingTop="2%" fontWeight="bold" mb={5}>
         Access Token
       </Text>
       <Stack spacing={6} padding="2% 0%">
-        <Flex>
-          <Flex w="30%" justifyContent="start" alignItems="center">
+        <Flex direction={isNotSmallerScreen ? "row" : "column"}>
+          <Flex
+            w={isNotSmallerScreen ? "30%" : "50%"}
+            justifyContent="start"
+            alignItems="center"
+          >
             <Text fontSize="sm">Access Token Expiry Time:</Text>
           </Flex>
-          <Flex w="70%">
+          <Flex
+            w={isNotSmallerScreen ? "70%" : "100%"}
+            mt={isNotSmallerScreen ? "0" : "3"}
+          >
             <InputField
+              borderRadius={5}
               variables={variables}
               setVariables={setVariables}
               inputType={TextInputType.ACCESS_TOKEN_EXPIRY_TIME}
@@ -24,12 +33,21 @@ const AccessToken = ({ variables, setVariables }: any) => {
             />
           </Flex>
         </Flex>
-        <Flex>
-          <Flex w="30%" justifyContent="start" direction="column">
+        <Flex direction={isNotSmallerScreen ? "row" : "column"}>
+          <Flex
+            w={isNotSmallerScreen ? "30%" : "60%"}
+            justifyContent="start"
+            direction="column"
+          >
             <Text fontSize="sm">Custom Scripts:</Text>
-            <Text fontSize="sm">Used to add custom fields in ID token</Text>
+            <Text fontSize="xs" color="blackAlpha.500">
+              (Used to add custom fields in ID token)
+            </Text>
           </Flex>
-          <Flex w="70%">
+          <Flex
+            w={isNotSmallerScreen ? "70%" : "100%"}
+            mt={isNotSmallerScreen ? "0" : "3"}
+          >
             <InputField
               variables={variables}
               setVariables={setVariables}
