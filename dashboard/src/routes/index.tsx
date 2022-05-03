ebccfb18cd1647f5d1074b1486ab30dcb736725d
akash.dutta <akash.dutta@glosity.club>
2022-05-03 19:50:01 +0530

    test-pull req

--- dashboard/src/routes/index.tsx
+++ dashboard/src/routes/index.tsx
@@ -23,10 +23,12 @@ export const AppRoutes = () => {
 							</DashboardLayout>
 						}
 					>
-						<Route path="/" element={<Environment />} />
-						<Route path="users" element={<Users />} />
-						<Route path="environment" element={<Environment />} />
-						<Route path="*" element={<Home />} />
+						<Route path="/" element={<Outlet />}>
+              <Route index element={<Environment />} />
+              <Route path="/:sec" element={<Environment />} />
+            </Route>
+            <Route path="users" element={<Users />} />
+            <Route path="*" element={<Home />} />
 					</Route>
 				</Routes>
 			</Suspense>
