25cb9a831be457beb412d4f6c451390b2747a1c4
Lakhan Samani <lakhan.m.samani@gmail.com>
2022-09-26 23:50:24 +0530

    feat: add cursor for pagination

--- server/graph/model/models_gen.go
+++ server/graph/model/models_gen.go
@@ -184,15 +184,17 @@ type PaginatedInput struct {
 }
 
 type Pagination struct {
-	Limit  int64 `json:"limit"`
-	Page   int64 `json:"page"`
-	Offset int64 `json:"offset"`
-	Total  int64 `json:"total"`
+	Limit  int64   `json:"limit"`
+	Page   int64   `json:"page"`
+	Cursor *string `json:"cursor"`
+	Offset int64   `json:"offset"`
+	Total  int64   `json:"total"`
 }
 
 type PaginationInput struct {
-	Limit *int64 `json:"limit"`
-	Page  *int64 `json:"page"`
+	Limit  *int64  `json:"limit"`
+	Page   *int64  `json:"page"`
+	Cursor *string `json:"cursor"`
 }
 
 type ResendOTPRequest struct {
