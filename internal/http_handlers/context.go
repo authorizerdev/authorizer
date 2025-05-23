c39a298c0f455700d73e1ae537adf5a0ed300dc0
Lakhan Samani <lakhan.m.samani@gmail.com>
2025-05-23 08:13:27 +0530

    chore: fix context

--- internal/http_handlers/context.go
+++ internal/http_handlers/context.go
@@ -6,10 +6,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Define a custom type for context key
+type contextKey string
+
+const ginContextKey contextKey = "GinContextKey"
+
 // ContextMiddleware is a middleware to add gin context in context
 func (h *httpProvider) ContextMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		ctx := context.WithValue(c.Request.Context(), "GinContextKey", c)
+		ctx := context.WithValue(c.Request.Context(), ginContextKey, c)
 		c.Request = c.Request.WithContext(ctx)
 		c.Next()
 	}
--- internal/http_handlers/playground.go
+++ internal/http_handlers/playground.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
 )
 
 // PlaygroundHandler is the handler for the /playground route
@@ -13,24 +14,23 @@ func (h *httpProvider) PlaygroundHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var handlerFunc http.HandlerFunc
 
-		// disablePlayground := h.Config.DisablePlayground
+		disablePlayground := h.Config.DisablePlayground
 
 		// if env set to false, then check if logged in as super admin, if logged in then return graphql else 401 error
 		// if env set to true, then disabled the playground with 404 error
-		// if !disablePlayground {
-		// 	if h.TokenProvider.IsSuperAdmin(c) {
-		// 		handlerFunc = playground.Handler("GraphQL", "/graphql")
-		// 	} else {
-		// 		log.Debug().Msg("not logged in as super admin")
-		// 		c.JSON(http.StatusUnauthorized, gin.H{"error": "not logged in as super admin"})
-		// 		return
-		// 	}
-		// } else {
-		// 	log.Debug().Msg("playground is disabled")
-		// 	c.JSON(http.StatusNotFound, gin.H{"error": "playground is disabled"})
-		// 	return
-		// }
-		handlerFunc = playground.Handler("GraphQL", "/graphql")
+		if !disablePlayground {
+			if h.TokenProvider.IsSuperAdmin(c) {
+				handlerFunc = playground.Handler("GraphQL", "/graphql")
+			} else {
+				log.Debug().Msg("not logged in as super admin")
+				c.JSON(http.StatusUnauthorized, gin.H{"error": "not logged in as super admin"})
+				return
+			}
+		} else {
+			log.Debug().Msg("playground is disabled")
+			c.JSON(http.StatusNotFound, gin.H{"error": "playground is disabled"})
+			return
+		}
 		handlerFunc.ServeHTTP(c.Writer, c.Request)
 	}
 }
