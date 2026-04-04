85c432f358ad7a9686323030eaad727d13cfb5ad
Lakhan Samani <lakhan.m.samani@gmail.com>
2026-04-04 15:56:33 +0530

    feat(rate-limit): add Redis sliding-window implementation

--- internal/rate_limit/redis_stub.go
+++
@@ -1,8 +0,0 @@
-package rate_limit
-
-import "github.com/authorizerdev/authorizer/internal/config"
-
-func newRedisProvider(cfg *config.Config, deps *Dependencies) (*inMemoryProvider, error) {
-	// Stub: will be replaced by redis.go in Task 3
-	return newInMemoryProvider(cfg, deps)
-}
